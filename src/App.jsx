996aa3f6d516d418c33c4819322058ca1bd1c5f1
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-02-11 17:15:50 -0300

    Se modifica mensaje para desarrollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { API_URL } from "./../env";
 
-
 const App = () => {
   return (
     <div>
       <h1>Hola {API_URL}</h1>
+      <p>Este es el sitio en desarrollo</p>
     </div>
   );
 };
